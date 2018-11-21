a077ccb81c1f7b8f53612167ecfcb91cbcc0394a
Leonardo Holanda <leoholanda23@gmail.com>
2018-11-20 22:48:56 -0200

    Comitando classe de teste

---
+++ src/main/java/classeMain.java
@@ -0,0 +1,18 @@
+
+public class classeMain {
+
+	public static void main(String[] args) {
+
+		int x = 1;
+
+		double y = ++x;
+
+		long z = (long) y;
+		
+		System.out.println(">>> X:" + x);
+		System.out.println(">>> Y:" + y);
+		System.out.println(">>> Z:" + z);
+
+	}
+
+}
