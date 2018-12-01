49bdda566fb2715050791530f5544fd0c1f10fa0
Leonardo Holanda <leoholanda23@gmail.com>
2018-12-01 20:21:28 -0200

    Teste de amend para novo commit

---
+++ src/test/java/TestConnectionRest.java
@@ -0,0 +1,12 @@
+public class TestConnectionRest {
+	
+//	@Test
+//	public void testaQueAConexaoComOServidorFunciona() {
+//		Client client = ClientBuilder.newClient();
+//		WebTarget target = client.target("http://www.mocky.io");
+//		String conteudo = target.path("/v2/5bf0c1de2f0000971a7a0cf4").request().get(String.class);
+//		System.out.println(conteudo);
+//		Assert.assertTrue(conteudo.contains("Joana"));
+//	}
+
+}
